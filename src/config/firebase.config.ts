b0089d1b22c38c3fd1643a6b5faeccb812cf4a5b
Vinicius Ribeiro <viniciusgoes159@gmail.com>
2024-02-19 14:46:58 -0300

    chore: add firebase settings and scripts

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDzIQtfYd2p8DNW-hUofoAvUyprml8sqys",
+    authDomain: "club-ecommerce-3ba92.firebaseapp.com",
+    projectId: "club-ecommerce-3ba92",
+    storageBucket: "club-ecommerce-3ba92.appspot.com",
+    messagingSenderId: "150739181421",
+    appId: "1:150739181421:web:8915cbc4ab0fda2ef48464"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
