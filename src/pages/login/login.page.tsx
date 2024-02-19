f336050c27a40ca3fa18fef373ecaedc96a55363
Vinicius Ribeiro <viniciusgoes159@gmail.com>
2024-02-19 17:36:01 -0300

    feat: login page with custom button and input

---
+++ src/pages/login/login.page.tsx
@@ -0,0 +1,44 @@
+
+import { BsGoogle } from 'react-icons/bs'
+import { FiLogIn } from 'react-icons/fi'
+
+import CustomButton from '../../components/custom-button/custom-button.component'
+import CustomInput from '../../components/custom-input/custom-input.component'
+import Header from '../../components/header/header.component'
+
+import {
+    LoginContainer,
+    LoginContent,
+    LoginHeadline,
+    LoginInputContainer,
+    LoginSubtitle
+} from './login.styles'
+
+const LoginPage = () => {
+    return (
+        <>
+            <Header />
+
+            <LoginContainer>
+                <LoginContent>
+                    <LoginHeadline>Entre com a sua conta</LoginHeadline>
+
+                    <CustomButton startIcon={<BsGoogle size={18} />} >Entrar com o Google</CustomButton>
+
+                    <LoginSubtitle>ou entre com o seu e-mail</LoginSubtitle>
+
+                    <LoginInputContainer>
+                        <CustomInput placeholder="Digite seu e-mail" />
+                    </LoginInputContainer>
+                    <LoginInputContainer>
+                        <CustomInput placeholder="Digite sua senha" />
+                    </LoginInputContainer>
+
+                    <CustomButton startIcon={<FiLogIn size={18} />} >Entrar</CustomButton>
+                </LoginContent>
+            </LoginContainer>
+        </>
+    )
+}
+
+export default LoginPage
