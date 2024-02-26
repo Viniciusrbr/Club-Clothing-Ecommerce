f7edea6e0c79b40dbb78b65625a2d040e5ac9606
Vinicius Ribeiro <viniciusgoes159@gmail.com>
2024-02-26 15:14:53 -0300

    feat: add User Firestore Converter

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import { onAuthStateChanged } from 'firebase/auth'
 import { auth, db } from './config/firebase.config'
 import { UserContext } from './contexts/user.context'
 import { collection, getDocs, query, where } from 'firebase/firestore'
+import { userConverter } from './converters/firestore.converters'
 
 const App: FunctionComponent = () => {
   const [isInitializing, setIsInitializing] = useState(true)
@@ -28,12 +29,13 @@ const App: FunctionComponent = () => {
 
     if (isSigningIn) {
       const querySnapshot = await getDocs(
-        query(collection(db, 'users'), where('id', '==', user?.uid))
+        query(collection(db, 'users').withConverter(userConverter),
+          where('id', '==', user?.uid))
       )
 
       const userFromFirestore = querySnapshot.docs[0]?.data()
 
-      loginUser(userFromFirestore as any)
+      loginUser(userFromFirestore)
 
       return setIsInitializing(false)
     }
