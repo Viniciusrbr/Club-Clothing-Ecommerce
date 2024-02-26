882f6320c42361db38fae705ce5a91c42b2920e8
Vinicius Ribeiro <viniciusgoes159@gmail.com>
2024-02-26 16:06:08 -0300

    feat: add Category Context

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+
 import UserContextProvider from './contexts/user.context.tsx'
+import CategoryContextProvider from './contexts/category.context'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <UserContextProvider>
-      <App />
+      <CategoryContextProvider>
+        <App />
+      </CategoryContextProvider>
     </UserContextProvider>
   </React.StrictMode>,
 )
