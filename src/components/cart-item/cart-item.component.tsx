67c7aa34c8d634d2b58fc44969931a2df28043b3
Vinicius Ribeiro <viniciusgoes159@gmail.com>
2024-02-28 15:58:33 -0300

    feat: add Cart Item Component with add and remove options

---
+++ src/components/cart-item/cart-item.component.tsx
@@ -0,0 +1,51 @@
+import { FunctionComponent, useContext } from 'react'
+import { AiOutlinePlus, AiOutlineMinus, AiOutlineClose } from 'react-icons/ai'
+import { CartContext } from '../../contexts/cart.context'
+
+// Utilities
+import CartProduct from '../../types/cart.types'
+
+// Styles
+import {
+    CartItemContainer,
+    CartItemImage,
+    CartItemInfo,
+    CartItemQuantity,
+    RemoveButton
+} from './cart-item.styles'
+
+interface CartItemProps {
+    product: CartProduct
+}
+
+const CartItem: FunctionComponent<CartItemProps> = ({ product }) => {
+
+    const { removeProductFromCart } = useContext(CartContext)
+
+    const handleRemoveClick = () => {
+        removeProductFromCart(product.id)
+    }
+
+    return (
+        <CartItemContainer>
+            <CartItemImage imageUrl={product.imageUrl} />
+
+            <CartItemInfo>
+                <p>{product.name}</p>
+                <p>R${product.price}</p>
+
+                <CartItemQuantity>
+                    <AiOutlineMinus size={20} />
+                    <p>{product.quantity}</p>
+                    <AiOutlinePlus size={20} />
+                </CartItemQuantity>
+            </CartItemInfo>
+
+            <RemoveButton onClick={handleRemoveClick}>
+                <AiOutlineClose size={25} />
+            </RemoveButton>
+        </CartItemContainer>
+    )
+}
+
+export default CartItem
--- src/contexts/cart.context.tsx
+++ src/contexts/cart.context.tsx
@@ -1,28 +1,61 @@
 import { createContext, FunctionComponent, useState } from 'react'
 import CartProduct from '../types/cart.types'
+import Product from '../types/product.types'
 
 interface ICartContext {
     isVisible: boolean
     products: CartProduct[]
     toggleCart: () => void
+    addProductToCart: (product: Product) => void
+    removeProductFromCart: (productId: string) => void
 }
 
 export const CartContext = createContext<ICartContext>({
     isVisible: false,
     products: [],
-    toggleCart: () => { }
+    toggleCart: () => { },
+    addProductToCart: () => { },
+    removeProductFromCart: () => { }
 })
 
 const CartContextProvider: FunctionComponent = ({ children }) => {
     const [isVisible, setIsVisible] = useState(false)
-    const [products] = useState<CartProduct[]>([])
+    const [products, setProducts] = useState<CartProduct[]>([])
 
     const toggleCart = () => {
         setIsVisible((prevState) => !prevState)
     }
 
+    const addProductToCart = (product: Product) => {
+        // verificar se o produto já está no carrinho
+        const productIsAlreadyInCart = products.some(
+            (item) => item.id === product.id
+        )
+
+        // se sim -> aumentar sua quantidade
+        if (productIsAlreadyInCart) {
+            return setProducts((products) =>
+                products.map((item) =>
+                    item.id === product.id
+                        ? { ...item, quantity: item.quantity + 1 }
+                        : item
+                )
+            )
+        }
+
+        // se não -> adicioná-lo
+        setProducts((prevState) => [...prevState, { ...product, quantity: 1 }])
+    }
+
+    const removeProductFromCart = (productId: string) => {
+        setProducts((products) =>
+            products.filter((product) => product.id !== productId)
+        )
+    }
+
     return (
-        <CartContext.Provider value={{ isVisible, products, toggleCart }}>
+        <CartContext.Provider
+            value={{ isVisible, products, toggleCart, addProductToCart, removeProductFromCart }}>
             {children}
         </CartContext.Provider>
     )
